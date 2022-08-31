bed74435fdac1dc49f6f700aa0292586a1471c31
BingoW <bingow@outlook.com>
2022-09-01 03:05:03 +0800

    22.9.1 3:05

--- luxury.js
+++ luxury.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        New script - a.luxury
 // @namespace   Violentmonkey Scripts
-// @match       http://[a-z].luxury/*
+// @match       http://*.luxury/*
 // @grant       none
 // @version     1.0
 // @author      -
@@ -21,14 +21,40 @@ function autoLogin() {
 
     document.querySelector("form button[type='button']").click();
     console.log("点击登录按钮完成。")
-    waitAndClick("div.el-message-box__btns button");
+    waitForBtn("div.el-message-box__btns button")
+        .then((btnElem) => {
+            btnElem.click()
+            console.log("点击登录成功按钮完成。")
+        });
     // 登录后，首页弹窗的“确定”按钮（并不确定一定会出现）。
-    console.log("登录完毕");
-    waitAndClick("div.el-dialog button.el-button");
+    console.log("登录完毕，开始尝试点击弹窗按钮。");
+    waitForBtn("div.el-dialog button.el-button")
+        .then((btnElem) => {
+            btnElem.click()
+            console.log("点击弹窗按钮完成。")
+        });
 }
 
 function autoCheckin() {
     console.log("自动签到开始。");
+    waitForBtn("div.el-dialog button.el-button")
+        .then((btnElem) => {
+
+            console.log("点击弹窗按钮开始。");
+            setTimeout(() => {
+                btnElem.click();
+                console.log("点击弹窗按钮完成。");
+            }, 2000);
+        })
+        .then(waitForBtn("div.leftbuttonwraps div:nth-last-child(1) a#succedaneum")
+            .then((btnElem) => {
+
+                console.log("点击签到按钮开始。");
+                setTimeout(() => {
+                    btnElem.click();
+                    console.log("点击签到按钮完成。");
+                }, 2000);
+            }));
 }
 
 function myInput(selector, str) {
@@ -37,8 +63,8 @@ function myInput(selector, str) {
     elem.dispatchEvent(new Event("input", { bubbles: true }));
 }
 
-function waitAndClick(btnSelector, parentSelector) {
-    new Promise((resolve, reject) => {
+function waitForBtn(btnSelector, parentSelector) {
+    return new Promise((resolve, reject) => {
         let btnElem = document.querySelector(btnSelector);
         if (btnElem) resolve(btnElem);
 
@@ -47,7 +73,7 @@ function waitAndClick(btnSelector, parentSelector) {
             if (btnElem) {
                 return resolve(btnElem);
             } else {
-                console.log("btn不存在。")
+                console.log("此轮检查发现btn暂不存在。")
             };
         });
 
@@ -55,9 +81,5 @@ function waitAndClick(btnSelector, parentSelector) {
         config = { childList: true, subtree: true };
         observer.observe(parentElem, config);
     })
-        .then((btnElem) => {
-            btnElem.click()
-            console.log("点击登录成功按钮完成。")
-        })
         .catch((error) => console.log(error));
 }
